50b995e3a1940740cc180b39989c32ebd858ddbd
IKyun Jin <greidron@gmail.com>
2023-12-31 23:54:47 +0900

    add navigation URI prefix

--- lib/get-canonical-page-id.ts
+++ lib/get-canonical-page-id.ts
@@ -1,10 +1,28 @@
 import { ExtendedRecordMap } from 'notion-types'
 import {
   getCanonicalPageId as getCanonicalPageIdImpl,
-  parsePageId
+  parsePageId,
+  idToUuid,
+  uuidToId,
 } from 'notion-utils'
 
-import { inversePageUrlOverrides } from './config'
+import { inversePageUrlOverrides, navigationPageIds } from './config'
+import { getBlockParent } from './get-block-parent'
+
+function getParentPath(
+  pageId: string,
+  recordMap: ExtendedRecordMap,
+): string | null {
+  const currentBlock = recordMap.block[idToUuid(pageId)]?.value
+  const navigationPageUuids = navigationPageIds.map(idToUuid)
+  const parentBlock = getBlockParent(
+    currentBlock,
+    recordMap,
+    (b) => navigationPageUuids.includes(b?.id)
+  )
+  const parentId = parentBlock?.id
+  return parentId && inversePageUrlOverrides[uuidToId(parentId)]
+}
 
 export function getCanonicalPageId(
   pageId: string,
@@ -20,8 +38,10 @@ export function getCanonicalPageId(
   if (override) {
     return override
   } else {
-    return getCanonicalPageIdImpl(pageId, recordMap, {
+    const parentPath = getParentPath(cleanPageId, recordMap)
+    const canonicalPageId = getCanonicalPageIdImpl(pageId, recordMap, {
       uuid
     })
+    return parentPath? parentPath + '/' + canonicalPageId : canonicalPageId
   }
 }
